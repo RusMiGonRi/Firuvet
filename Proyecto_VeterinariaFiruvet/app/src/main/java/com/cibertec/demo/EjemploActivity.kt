41625d53b5cec6ea1f65ad4b921935c6899714bf
RusMiGonRi <rusber.gori@gmail.com>
2026-02-04 21:28:05 -0500

    Ejemplo numero 3 del Proyecto

---
+++ Proyecto_VeterinariaFiruvet/app/src/main/java/com/cibertec/demo/EjemploActivity.kt
@@ -0,0 +1,20 @@
+package com.cibertec.demo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class EjemploActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ejemplo)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
