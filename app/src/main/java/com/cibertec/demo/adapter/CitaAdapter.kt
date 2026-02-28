2a7a2f4b35a15324de7eabbe898a7b7567badaa4
RusMiGonRi <rusber.gori@gmail.com>
2026-02-28 13:14:12 -0500

    VERSION | 9.0.4.0 - Agregado el adaptador CitaAdapter - Agregado el item ItemCita

---
+++ app/src/main/java/com/cibertec/demo/adapter/CitaAdapter.kt
@@ -0,0 +1,32 @@
+package com.cibertec.demo.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.cibertec.demo.R
+import com.cibertec.demo.entity.Cita
+
+class CitaAdapter(private val listaCitas: List<Cita>) :
+    RecyclerView.Adapter<CitaAdapter.CitaViewHolder>() {
+
+    class CitaViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvMascota: TextView = view.findViewById(R.id.tvMascotaCita)
+        val tvFechaHora: TextView = view.findViewById(R.id.tvFechaHoraCita)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CitaViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_cita, parent, false)
+        return CitaViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CitaViewHolder, position: Int) {
+        val cita = listaCitas[position]
+        holder.tvMascota.text = cita.mascota
+        holder.tvFechaHora.text = "${cita.fecha} - ${cita.hora}"
+    }
+
+    override fun getItemCount() = listaCitas.size
+}
